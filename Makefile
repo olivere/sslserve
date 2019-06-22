3e766bcb6e29f1bef1d9f9bcbe10279e127efd26
Oliver Eilhard <oliver@eilhard.net>
2019-06-22 14:39:31 +0200

    Make WebSockets work
    This commit illustrates how to proxy WebSockets as well.

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ default: build
 
 build:
 	GOOS=linux CGO_ENABLED=0 go build -o service
+
+restart:
+	docker-compose up -d --no-deps --build service
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -13,16 +14,28 @@ import (
 
 var (
 	hostname string
-
-	upgrader = websocket.Upgrader{}
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:    4096,
+		WriteBufferSize:   4096,
+		EnableCompression: true,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	tmpl = template.Must(template.New("").Parse(homeTemplate))
 )
 
 func main() {
-	hostname, _ = os.Hostname()
+	var err error
+
+	hostname, err = os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
-	r.HandleFunc("/websocket", ws)
+	r.HandleFunc("/ws", ws)
 	httpSrv := &http.Server{
 		Addr:    ":3000",
 		Handler: r,
@@ -32,7 +45,18 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-	fmt.Fprintf(w, "Hostname: %s\nCurrent time is %v in %v\n", hostname, now, now.Location())
+	err := tmpl.Execute(w, struct {
+		Hostname string
+		Time     string
+		Location string
+	}{
+		Hostname: hostname,
+		Time:     now.Format(time.RFC3339),
+		Location: now.Location().String(),
+	})
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
 }
 
 func ws(w http.ResponseWriter, r *http.Request) {
@@ -57,3 +81,74 @@ func ws(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+var (
+	homeTemplate = `<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>SSLServe</title>
+</head>
+<body>
+<h1>SSLServe</h1>
+<div>
+	<p>Hostname: {{.Hostname}}</p>
+	<p>
+		Time: {{.Time}}<br>
+		Location: {{.Location}}
+	</p>
+</div>
+
+<h1>WebSocket</h1>
+<form>
+	<p>
+		<input type="text" id="input" value="Hello" />
+		<button id="send">Send</button>
+	</p>
+</form>
+<div id="output"></div>
+<script>
+window.addEventListener("load", function(evt) {
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.innerHTML = message;
+        output.appendChild(d);
+    };
+    var open = function() {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("wss://localhost:443/ws");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+	};
+	
+	open();
+});
+</script>
+</body>
+</html>`
+)
